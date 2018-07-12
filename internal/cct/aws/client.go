6768b0dd13b8cb0e300fe4fbf8a11d75960ae2ea
jakobl <jakobl@cs.umu.se>
2018-07-12 16:56:51 +0200

    the aws client will automatically select the appropriate report

--- internal/cct/aws/client.go
+++ internal/cct/aws/client.go
@@ -3,7 +3,6 @@ package aws
 
 import (
 	"encoding/csv"
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -19,12 +18,11 @@ import (
 // Client ...
 type Client struct {
 	bucket string
-	key    string
 }
 
 // NewClient ...
-func NewClient(bucket string, key string) Client {
-	client := Client{bucket: bucket, key: key}
+func NewClient(bucket string) Client {
+	client := Client{bucket: bucket}
 	return client
 }
 
@@ -89,66 +87,6 @@ func getTable(bucket string, key string, query string) [][]string {
 	return tbl
 }
 
-//func s3ToUnix(timestamp string, form string) int {
-//	tmp, err := time.Parse(form, timestamp)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	unix := strconv.FormatInt(tmp.Unix(), 10)
-//	unixI, err := strconv.Atoi(unix)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	return unixI
-//}
-
-// GetAccumulatedCost ...
-//func GetAccumulatedCost(bucket string, key string, timestamp time.Time) []interface{} {
-//
-//	// Query to be used by S3 Select
-//	query := "SELECT s._11, s._12, s._13, s._24 FROM S3Object s"
-//
-//	// Get table from S3
-//	tbl := getTable(bucket, key, query)
-//
-//	// Define S3 timestamp format using Go reference time
-//	form := "2006-01-02T15:04:05Z"
-//	baz := strconv.FormatInt(timestamp.Unix(), 10)
-//
-//	// Transform
-//	for _, val := range tbl {
-//		a := s3ToUnix(val[0], form)
-//		b := s3ToUnix(val[1], form)
-//		x, _ := strconv.Atoi(baz)
-//		y := calculateOverlap(a, b, x)
-//		tmp, _ := strconv.ParseFloat(val[3], 64)
-//		val[3] = strconv.FormatFloat(tmp*y, 'f', -1, 64)
-//	}
-//
-//	// Group by service
-//	m := map[string]float64{}
-//	for _, val := range tbl {
-//		res, _ := strconv.ParseFloat(val[3], 64)
-//		m[val[2]] += res
-//	}
-//
-//	// Convert back to slice
-//	res := make([]interface{}, 0)
-//	for key, cost := range m {
-//		res = append(res, key)
-//		res = append(res, cost)
-//	}
-//
-//	return res
-//}
-
-//func calculateOverlap(a int, b int, x int) float64 {
-//	y := (float64(x) - float64(a)) / (float64(b) - float64(a))
-//	y = math.Max(y, 0.0)
-//	y = math.Min(y, 1.0)
-//	return y
-//}
-
 func overlap(a int64, b int64, c int64, d int64) float64 {
 	return math.Max(0, math.Min(float64(b), float64(d))-math.Max(float64(a), float64(c)))
 }
@@ -168,15 +106,19 @@ func (client *Client) GetCloudCost(timestamp time.Time) ([]dbclient.UsageData, e
 
 	form := "2006-01-02T15:04:05Z"
 
+	// Calculate the correct key
+	key := getBucketKey(client.bucket, timestamp)
+
 	// Get table from bucket with key using query
-	tbl := getTable(client.bucket, client.key, query)
+	tbl := getTable(client.bucket, key, query)
 
 	// Transform result into internal format []UsageData
 	res := make([]dbclient.UsageData, 0)
 	for _, val := range tbl {
 		labels := map[string]string{}
-		labels["Service"] = val[2]
-		labels["Currency"] = val[3]
+		labels["service"] = val[2]
+		labels["currency"] = val[3]
+		labels["cloud"] = "aws"
 		start, _ := time.Parse(form, val[0])
 		stop, _ := time.Parse(form, val[1])
 		ratio := calculateRatio(start, stop, timestamp)
@@ -187,7 +129,16 @@ func (client *Client) GetCloudCost(timestamp time.Time) ([]dbclient.UsageData, e
 			Labels: labels}
 		res = append(res, row)
 	}
+
+	// Group similar UsageData
+	res = groupUsageData(res)
+
 	return res, nil
+}
+
+// TODO
+func groupUsageData(data []dbclient.UsageData) []dbclient.UsageData {
+	return data
 }
 
 func keyIsValid(key string, date string) bool {
@@ -210,14 +161,12 @@ func selectKey(objects []*s3.Object, date string) string {
 				latest = *val.LastModified
 				key = *val.Key
 			}
-			// fmt.Println(*val.Key + " " + val.LastModified.String())
 		}
 	}
 	return key
 }
 
-// GetBucketKey ...
-func GetBucketKey(bucket string, timestamp time.Time) string {
+func getBucketKey(bucket string, timestamp time.Time) string {
 	svc := newS3Service()
 	params := &s3.ListObjectsInput{
 		Bucket: aws.String(bucket),
@@ -227,13 +176,12 @@ func GetBucketKey(bucket string, timestamp time.Time) string {
 	start := time.Date(timestamp.Year(), timestamp.Month(), 1, 0, 0, 0, 0, timestamp.Location())
 	stop := start.AddDate(0, 1, 0)
 
-	// 20180701-20180801
 	form := "20060102"
 	date := start.Format(form) + "-" + stop.Format(form)
 
 	resp, _ := svc.ListObjects(params)
 
-	fmt.Println(selectKey(resp.Contents, date))
+	key := selectKey(resp.Contents, date)
 
-	return ""
+	return key
 }
